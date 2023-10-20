1b05a037cf7b76ca3fd63c8910abdcd6d8d44425
Stanislav <stanjke@gmail.com>
2023-10-21 01:55:16 +0300

    clen branch dev from errors

--- client/src/components/Button/Button.jsx
+++ client/src/components/Button/Button.jsx
@@ -3,7 +3,9 @@ import PropTypes from 'prop-types';
 import styles from './Button.module.scss';
 
 function Button(props) {
-  const { type, text, className, onClick } = props;
+  const {
+    type, text, className, onClick,
+  } = props;
 
   const buttonClasses = [
     styles.btn,
@@ -12,6 +14,7 @@ function Button(props) {
   ].join(' ');
 
   return (
+    // eslint-disable-next-line react/button-has-type
     <button type={type} className={buttonClasses} onClick={onClick}>
       {text}
     </button>
