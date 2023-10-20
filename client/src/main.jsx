59c0aef61015e6328b136e9776263feaea035210
Stanislav <stanjke@gmail.com>
2023-10-20 17:45:36 +0300

    configured base project configes, fixed problem with tests, added prittier settings

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,13 +1,12 @@
-// import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { Provider } from 'react-redux/es/exports'
-import { store } from './store/index.js'
-import App from './App.jsx'
-import './index.css'
-
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux/es/exports';
+import store from './store';
+import App from './App';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <App />
-  </Provider>
-)
+  </Provider>,
+);
