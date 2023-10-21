962bcd2660c94e7858a7b156302d23b768f66822
Stanislav <stanjke@gmail.com>
2023-10-21 23:06:40 +0300

    Added empty pages components, made routers to it, added Footer component as test

---
+++ client/src/components/Footer/Footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import styles from './Footer.module.scss';
+
+function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.footer__main}>
+        <span>123</span>
+        <div className={styles.footer__main__content}>a</div>
+      </div>
+      <div className={styles.footer__secondary}>secondary</div>
+    </footer>
+  );
+}
+
+export default Footer;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux/es/exports';
+import { BrowserRouter } from 'react-router-dom';
 import store from './store';
 import App from './App';
+
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+
   <Provider store={store}>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </Provider>,
+
 );
