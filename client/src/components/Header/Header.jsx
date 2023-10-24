fdd85ddaa08c0782df309fbfc8e70d904fa04b77
Vitaliy Snezhkin <vpsnejok@gmail.com>
2023-10-24 12:55:24 +0300

    import icons

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -8,55 +8,56 @@ import CartIcon from './icons/CartIcon';
 import LoginIcon from './icons/LoginIcon';
 import Container from '../Container/Container';
 
+
 import './Header.scss';
 
 function Header() {
   const [show, setShow] = useState(false);
   return (
-    <header className="header">
+    <header className='header'>
       <Container>
-        <div className="wrapp">
+        <div className='wrapp'>
           {/* eslint-disable-next-line react/button-has-type */}
-          <button className="toggleBtn" onClick={() => setShow(!show)}>
-            {show ? <span className="fa fa-times">da</span>
-              : <span className="fa fa-bars">net</span>}
+          <button className='toggleBtn' onClick={() => setShow(!show)}>
+            {show ? <span className='fa fa-times'>da</span>
+              : <span className='fa fa-bars'>net</span>}
           </button>
-          <Link to="/">
+          <Link to='/'>
             <LogoIcon />
-            <span className="header_logo-title">Herba</span>
-            <span className="header_logo-title accent-color">List</span>
+            <span className='header_logo-title'>Herba</span>
+            <span className='header_logo-title accent-color'>List</span>
           </Link>
           <nav className={`${show ? 'mobile-nav' : 'list'}`}>
-            <ul className="header_nav-item">
-              <li className="header_nav-item">
-                <Link to="/">Home</Link>
+            <ul className='header_nav-item'>
+              <li className='header_nav-item'>
+                <Link to='/'>Home</Link>
               </li>
-              <li className="header_nav-item">
-                <Link to="/shop">Shop</Link>
+              <li className='header_nav-item'>
+                <Link to='/shop'>Shop</Link>
               </li>
-              <li className="header_nav-item">
-                <Link to="/news">News</Link>
+              <li className='header_nav-item'>
+                <Link to='/news'>News</Link>
               </li>
-              <li className="header_nav-item">
-                <Link to="/pages">Pages</Link>
+              <li className='header_nav-item'>
+                <Link to='/pages'>Pages</Link>
               </li>
             </ul>
           </nav>
-          <div className="header_user">
-            <form className="header_user-search">
-              <input className="header_user-serarch_input" type="text" />
+          <div className='header_user'>
+            <form className='header_user-search'>
+              <input className='header_user-serarch_input' type='text' />
               {/* eslint-disable-next-line react/button-has-type */}
-              <button className="header_user-serarch_button">
+              <button className='header_user-serarch_button'>
                 <SearchIcon />
               </button>
             </form>
-            <Link to="/login">
+            <Link to='/login'>
               <LoginIcon />
             </Link>
-            <Link to="/wishlist">
+            <Link to='/wishlist'>
               <WishlistIcon />
             </Link>
-            <Link to="/cart">
+            <Link to='/cart'>
               <CartIcon />
             </Link>
           </div>
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,17 +1,13 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import svgr from 'vite-plugin-svgr';
+import svg from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    svgr({
-      svgrOptions: {
-        // svgr options
-      },
-    }),
+    svg(),
   ],
   css: {
     preprocessorOptions: {
