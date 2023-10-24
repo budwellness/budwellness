503a6d593ea1c350c4b922b2d91146abb19c3e4a
User <tatiana.naumchuk123456789@gmail.com>
2023-10-24 22:37:18 +0300

    add NavLink

--- client/src/components/PagePreviewHeader/PagePreviewHeader.jsx
+++ client/src/components/PagePreviewHeader/PagePreviewHeader.jsx
@@ -1,15 +1,24 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import styles from './PagePreviewHeader.module.scss';
 
 function PagePreviewHeader({ title, text }) {
   //   const { title, text } = props;
   return (
-    <div className={styles.wrapper}>
-      <div className={styles.container}>
-        <h1 className={styles.title}>{title}</h1>
-        <p className={styles.text}>{text}</p>
+    <>
+      <div className={styles.wrapper}>
+        <div className={styles.container}>
+          <h1 className={styles.title}>{title}</h1>
+          <p className={styles.text}>{text}</p>
+        </div>
       </div>
-    </div>
+      <div className={styles.link}>
+        <NavLink
+          to="/"
+          className={({ isActive }) => (isActive ? 'active-link' : '')}
+        />
+      </div>
+    </>
   );
 }
 
