e8e08c489ae59a21febfa954a65071e45030c83e
Vitaliy Snezhkin <vpsnejok@gmail.com>
2023-10-28 00:18:12 +0300

    Header, the adaptive is ready, the styles have been completed and burgermenu

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -25,7 +25,7 @@ function Header() {
             <span className={cn(styles.line, styles.shortLine)} />
             <span className={styles.line} />
           </span>
-          <Link to='/'>
+          <Link to="/">
             <LogoIcon />
             <span className={styles.header_logoTitle}>Bud</span>
             <span className={cn(styles.header_logoTitle, styles.accentColor)}>Wellness</span>
@@ -33,41 +33,41 @@ function Header() {
           <nav className={`${show ? styles.mobileNav : styles.list}`}>
             <ul className={styles.header_navList}>
               <li className={styles.header_navItem}>
-                  <Link to='/'>Home</Link>
-                </li>
-                <li className={styles.header_navItem}>
-                  <Link to='/shop'>Shop</Link>
-                </li>
-                <li className={styles.header_navItem}>
-                  <Link to='/news'>News</Link>
-                </li>
-                <li className={styles.header_navItem}>
-                  <Link to='/pages'>Pages</Link>
-                </li>
-              </ul>
-            </nav>
-            <div className={styles.header_user}>
-              <form className={styles.header_userSearchForm}>
-                <input className={styles.header_userSearchInput} type='text' placeholder='Search...' />
-                {/* eslint-disable-next-line react/button-has-type */}
-                <button className={styles.header_userSearchButton}>
-                  <SearchIcon />
-                </button>
-              </form>
-              <Link to='/login' className={styles.header_userLink}>
-                <LoginIcon />
-              </Link>
-              <Link to='/wishlist' className={styles.header_userLink}>
-                <WishlistIcon />
-              </Link>
-              <Link to='/cart' className={styles.header_userLink}>
-                <CartIcon />
-              </Link>
-            </div>
+                <Link to="/">Home</Link>
+              </li>
+              <li className={styles.header_navItem}>
+                <Link to="/shop">Shop</Link>
+              </li>
+              <li className={styles.header_navItem}>
+                <Link to="/news">News</Link>
+              </li>
+              <li className={styles.header_navItem}>
+                <Link to="/pages">Pages</Link>
+              </li>
+            </ul>
+          </nav>
+          <div className={styles.header_user}>
+            <form className={styles.header_userSearchForm}>
+              <input className={styles.header_userSearchInput} type="text" placeholder="Search..." />
+              {/* eslint-disable-next-line react/button-has-type */}
+              <button className={styles.header_userSearchButton}>
+                <SearchIcon />
+              </button>
+            </form>
+            <Link to="/login" className={styles.header_userLink}>
+              <LoginIcon />
+            </Link>
+            <Link to="/wishlist" className={styles.header_userLink}>
+              <WishlistIcon />
+            </Link>
+            <Link to="/cart" className={styles.header_userLink}>
+              <CartIcon />
+            </Link>
+          </div>
         </div>
       </Container>
     </header>
-);
+  );
 }
 
 export default Header;
