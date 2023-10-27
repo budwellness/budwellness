b9bc36d032b8d62f9d9acb2aad3497eafd59704a
Vitaliy Snezhkin <vpsnejok@gmail.com>
2023-10-28 00:16:46 +0300

    Header, the adaptive is ready, the styles have been completed and burgermenu

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -17,56 +17,57 @@ function Header() {
     <header className={styles.header}>
       <Container>
         <div className={styles.wrapp}>
-          {/* eslint-disable-next-line jsx-a11y/click-events-have-key-events,max-len */}
+          {/* eslint-disable-next-line max-len */}
+          {/* eslint-disable-next-line jsx-a11y/click-events-have-key-events,max-len,jsx-a11y/no-static-element-interactions */}
           <span className={cn(styles.toggleBtn, { [styles.active]: show })} onClick={() => setShow(!show)}>
             <span className={cn(styles.line, styles.shortLine)} />
             <span className={styles.line} />
             <span className={cn(styles.line, styles.shortLine)} />
             <span className={styles.line} />
           </span>
-          <Link to="/">
+          <Link to='/'>
             <LogoIcon />
-            <span className={styles.header_logoTitle}>Herba</span>
-            <span className={cn(styles.header_logoTitle, styles.accentColor)}>List</span>
+            <span className={styles.header_logoTitle}>Bud</span>
+            <span className={cn(styles.header_logoTitle, styles.accentColor)}>Wellness</span>
           </Link>
           <nav className={`${show ? styles.mobileNav : styles.list}`}>
             <ul className={styles.header_navList}>
               <li className={styles.header_navItem}>
-                <Link to='/'>Home</Link>
-              </li>
-              <li className={styles.header_navItem}>
-                <Link to='/shop'>Shop</Link>
-              </li>
-              <li className={styles.header_navItem}>
-                <Link to='/news'>News</Link>
-              </li>
-              <li className={styles.header_navItem}>
-                <Link to='/pages'>Pages</Link>
-              </li>
-            </ul>
-          </nav>
-          <div className={styles.header_user}>
-            <form className={styles.header_userSearchForm}>
-              <input className={styles.header_userSearchInput} type="text" placeholder="Search..." />
-              {/* eslint-disable-next-line react/button-has-type */}
-              <button className={styles.header_userSearchButton}>
-                <SearchIcon />
-              </button>
-            </form>
-            <Link to='/login' className={styles.header_userLink}>
-              <LoginIcon />
-            </Link>
-            <Link to='/wishlist' className={styles.header_userLink}>
-              <WishlistIcon />
-            </Link>
-            <Link to='/cart' className={styles.header_userLink}>
-              <CartIcon />
-            </Link>
-          </div>
+                  <Link to='/'>Home</Link>
+                </li>
+                <li className={styles.header_navItem}>
+                  <Link to='/shop'>Shop</Link>
+                </li>
+                <li className={styles.header_navItem}>
+                  <Link to='/news'>News</Link>
+                </li>
+                <li className={styles.header_navItem}>
+                  <Link to='/pages'>Pages</Link>
+                </li>
+              </ul>
+            </nav>
+            <div className={styles.header_user}>
+              <form className={styles.header_userSearchForm}>
+                <input className={styles.header_userSearchInput} type='text' placeholder='Search...' />
+                {/* eslint-disable-next-line react/button-has-type */}
+                <button className={styles.header_userSearchButton}>
+                  <SearchIcon />
+                </button>
+              </form>
+              <Link to='/login' className={styles.header_userLink}>
+                <LoginIcon />
+              </Link>
+              <Link to='/wishlist' className={styles.header_userLink}>
+                <WishlistIcon />
+              </Link>
+              <Link to='/cart' className={styles.header_userLink}>
+                <CartIcon />
+              </Link>
+            </div>
         </div>
       </Container>
     </header>
-  );
+);
 }
 
 export default Header;
