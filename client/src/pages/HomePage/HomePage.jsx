b76b343f96199da0dee294f908047144f82604ab
Diana Liashenko <leonidovna8@gmail.com>
2023-10-29 03:03:34 +0300

    created component and styles

--- client/src/pages/HomePage/HomePage.jsx
+++ client/src/pages/HomePage/HomePage.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Categories from '../../components/Categories/Categories';
+import DescriptionWeed from '../../components/DesctiptionWeed/DescriptionWeed';
 
 function HomePage() {
   return (
     <>
       <div>HomePage</div>
       <Categories />
+      <DescriptionWeed />
     </>
   );
 }
