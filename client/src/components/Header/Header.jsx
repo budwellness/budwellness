31d9f9fff1db98c762486fdb17cc3c6bb6ca2980
Vitaliy Snezhkin <vpsnejok@gmail.com>
2023-10-31 01:12:11 +0200

    update component modal, styles

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -11,66 +11,77 @@ import Container from '../Container/Container';
 
 import styles from './Header.module.scss';
 import Modal from '../Modal/Modal.jsx';
+import Footer from '../Footer/Footer.jsx';
 
 function Header() {
   const [showBurger, setShowBurger] = useState(false);
   const [showModal, setShowModal] = useState(false);
+
+  const handleModal = () => {
+    setShowModal(!showModal);
+  };
+
   return (
     <header className={styles.header}>
       <Container>
         <div className={styles.wrapp}>
           {/* eslint-disable-next-line max-len */}
-          {/* eslint-disable-next-line jsx-a11y/click-events-have-key-events,max-len,jsx-a11y/no-static-element-interactions */}
-          <span className={cn(styles.toggleBtn, { [styles.active]: showBurger })}
-                onClick={() => setShowBurger(!showBurger)}>
+          {/* eslint-disable-next-line jsx-a11y/click-events-have-key-events,jsx-a11y/no-static-element-interactions */}
+          <span
+            className={cn(styles.toggleBtn, { [styles.active]: showBurger })}
+            onClick={() => setShowBurger(!showBurger)}
+          >
             <span className={cn(styles.line, styles.shortLine)} />
             <span className={styles.line} />
             <span className={cn(styles.line, styles.shortLine)} />
             <span className={styles.line} />
           </span>
-          <Link to='/'>
+          <Link to="/">
             <LogoIcon />
             <span className={styles.header_logoTitle}>Bud</span>
             <span className={cn(styles.header_logoTitle, styles.accentColor)}>Wellness</span>
           </Link>
           <nav className={`${showBurger ? styles.mobileNav : styles.list}`}>
             <ul className={styles.header_navList}>
-              <li className={styles.header_navItem}>
-                <Link to='/'>Home</Link>
+              <li className={styles.header_navItem} onClick={() => setShowBurger(false)}>
+                <Link to="/">Home</Link>
               </li>
-              <li className={styles.header_navItem}>
-                <Link to='/shop'>Shop</Link>
+              <li className={styles.header_navItem} onClick={() => setShowBurger(false)}>
+                <Link to="/shop">Shop</Link>
               </li>
-              <li className={styles.header_navItem}>
-                <Link to='/news'>News</Link>
+              <li className={styles.header_navItem} onClick={() => setShowBurger(false)}>
+                <Link to="/news">News</Link>
               </li>
-              <li className={styles.header_navItem}>
-                <Link to='/pages'>Pages</Link>
+              <li className={styles.header_navItem} onClick={() => setShowBurger(false)}>
+                <Link to="/pages">Pages</Link>
               </li>
             </ul>
           </nav>
           <div className={styles.header_user}>
             <form className={styles.header_userSearchForm}>
-              <input className={styles.header_userSearchInput} type='text' placeholder='Search...' />
+              <input className={styles.header_userSearchInput} type="text" placeholder="Search..." />
               {/* eslint-disable-next-line react/button-has-type */}
               <button className={styles.header_userSearchButton}>
                 <SearchIcon />
               </button>
             </form>
-            <Link to='/login' className={styles.header_userLink}>
+            <Link to="/" className={styles.header_userLink} onClick={handleModal}>
               <LoginIcon />
             </Link>
-            <Link to='/wishlist' className={styles.header_userLink}>
+            <Link to="/wishlist" className={styles.header_userLink}>
               <WishlistIcon />
             </Link>
-            <Link to='/cart' className={styles.header_userLink}>
+            <Link to="/cart" className={styles.header_userLink}>
               <CartIcon />
             </Link>
           </div>
         </div>
       </Container>
-      {showModal && <Modal />}
-      <Modal />
+      {showModal && (
+        <Modal handleModal={handleModal}>
+          <div className='test'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab, accusamus accusantium autem dolor et illo iusto laboriosam magni maxime obcaecati?</div>
+        </Modal>
+      )}
     </header>
   );
 }
--- client/src/components/Modal/Modal.jsx
+++ client/src/components/Modal/Modal.jsx
@@ -1,21 +1,32 @@
 import React from 'react';
-
+import PropTypes from 'prop-types';
 import styles from './Modal.module.scss';
 
-
-function Modal({ closeModal }) {
-  const handleOutSideModal = ({ target }) => {
-    if (!target.closest('.modal')) {
-      return closeModal();
+function Modal({ handleModal, children }) {
+  const handleCloseModal = (e) => {
+    if (e.target === e.currentTarget) {
+      handleModal();
     }
   };
   return (
-    <div className={styles.modalWrapper} onClick={(event) => handleOutSideModal(event)}>
+    <div className={styles.modalWrapper} onClick={handleCloseModal}>
       <div className={styles.modal}>
-        <span className="close">&times;</span>
+       <span className={styles.close} onClick={handleModal}>
+          &times;
+        </span>
+        {children}
       </div>
     </div>
   );
 }
+
+Modal.defaultProps = {
+  children: null,
+};
+
+Modal.propTypes = {
+  handleModal: PropTypes.func.isRequired,
+  children: PropTypes.node,
+};
 
 export default Modal;
