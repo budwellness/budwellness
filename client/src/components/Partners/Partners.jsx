d22c9986a00322232f0d85465bac4c814305b0a5
Diana Liashenko <leonidovna8@gmail.com>
2023-10-30 10:47:05 +0200

    updated styles for partners

--- client/src/components/Partners/Partners.jsx
+++ client/src/components/Partners/Partners.jsx
@@ -4,19 +4,19 @@ import styles from './Partners.module.scss';
 function Partners() {
   return (
     <div className={styles.partners}>
-      <a className={styles.partnerHemp} href="https://www.hempcare.it/en/">
+      <a className={styles.partners__hemp} href="https://www.hempcare.it/en/">
         <img src="/public/images/partnerOne.png" alt="Hemp Care" />
       </a>
-      <a className={styles.partnerGreen} href="https://www.gtigrows.com/">
+      <a className={styles.partners__green} href="https://www.gtigrows.com/">
         <img src="/public/images/partnerTwo.png" alt="Green Thumb" />
       </a>
-      <a className={styles.partnerVerano} href="https://www.verano.com/">
+      <a className={styles.partners__verano} href="https://www.verano.com/">
         <img src="/public/images/partnerThree.png" alt="Verano" />
       </a>
-      <a className={styles.partnerHoly} href="https://www.holyherb.com/">
+      <a className={styles.partners__voly} href="https://www.holyherb.com/">
         <img src="/public/images/partnerFour.png" alt="Holy Herb" />
       </a>
-      <a className={styles.partnerCresco} href="https://www.crescolabs.com/">
+      <a className={styles.partners__cresco} href="https://www.crescolabs.com/">
         <img src="/public/images/partnerFive.png" alt="Cresco Labs" />
       </a>
     </div>
