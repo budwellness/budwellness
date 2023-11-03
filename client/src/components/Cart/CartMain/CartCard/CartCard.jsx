d245821c2717db713f17618915600b974a684e7d
Сергій Сокол <sokol.serhii@gmail.com>
2023-11-04 00:01:51 +0200

    Changed component Cart and create new component CloseIcon

--- client/src/components/Cart/CartMain/CartCard/CartCard.jsx
+++ client/src/components/Cart/CartMain/CartCard/CartCard.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import cN from 'classnames';
+
+import CloseIcon from '../../../UI/CloseIcon';
 
 import styles from './CartCard.module.scss';
 
@@ -22,6 +25,22 @@ export default function CartCard({ card }) {
             <span className={styles.itemInfo_volume}>??mg</span>
           </div>
         </div>
+        <div className={styles.wrapper_price}>
+          {card.previousPrice !== card.currentPrice && (
+            <span className={cN(styles.price, styles.priceOld)}>
+              ${card.previousPrice.toFixed(2)}
+            </span>
+          )}
+          <span className={styles.price}>${card.currentPrice.toFixed(2)}</span>
+        </div>
+        <div className={cN(styles.wrapper_price, styles.wrapper_priceSubtotal)}>
+          <h5 className={styles.priceSubtotal_title}>Subtotal</h5>
+          <span className={styles.priceSubtotal_total}>$??.??</span>
+        </div>
+        <div className={styles.quantity}>- 1 +</div>
+        <button type="button" className={styles.btnClose} onClick={() => {}}>
+          <CloseIcon className={styles.iconClose} />
+        </button>
       </div>
     </li>
   );
