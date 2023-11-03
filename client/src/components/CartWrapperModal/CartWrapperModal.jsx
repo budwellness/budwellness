98d0ffde8db8041eb5b8f7d00d801b1832b6af4e
Vitaliy Snezhkin <vpsnejok@gmail.com>
2023-11-04 00:58:53 +0200

    update cartWrapperModal, add Button

--- client/src/components/CartWrapperModal/CartWrapperModal.jsx
+++ client/src/components/CartWrapperModal/CartWrapperModal.jsx
@@ -1,12 +1,27 @@
 import React from 'react';
 import CartModalItem from './CartModalItem/CartModalItem';
+import Button from '../Button/Button';
+
+import styles from './CartWrapperModal.module.scss';
 
 function CartWrapperModal() {
   return (
-    <ul>
-      <CartModalItem />
-      <CartModalItem />
-    </ul>
+    <>
+      <ul>
+        <CartModalItem />
+        <CartModalItem />
+      </ul>
+      <div className={styles.footer}>
+        <span className={styles.footerTitle}>Total</span>
+        <span className={styles.footerPrice}>$29.43</span>
+      </div>
+      <Button
+        type="button"
+        text="Proceed to checkout"
+        className="orangeBtn"
+        onClick={() => {}}
+      />
+    </>
   );
 }
 
