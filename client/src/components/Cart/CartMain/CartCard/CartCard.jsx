f5c41dbba1da656bde1ebd5eadb2f14b5b638ecf
Сергій Сокол <sokol.serhii@gmail.com>
2023-11-07 15:43:00 +0200

    Changed data currentCurd

--- client/src/components/Cart/CartMain/CartCard/CartCard.jsx
+++ client/src/components/Cart/CartMain/CartCard/CartCard.jsx
@@ -15,29 +15,36 @@ export default function CartCard({ card }) {
             <picture>
               <img
                 className={styles.itemMediaImg}
-                src={card.imageUrls[0]}
-                alt={card.name}
+                src={card.product.imageUrls[0]}
+                alt={card.product.name}
               />
             </picture>
           </div>
           <div className={styles.wrapper_itemInfo}>
-            <h4 className={styles.itemInfo_title}>{card.name}</h4>
-            <span className={styles.itemInfo_volume}>??mg</span>
+            <h4 className={styles.itemInfo_title}>{card.product.name}</h4>
+            <span className={styles.itemInfo_volume}>
+              {card.product.size}
+              {card.product.sizeType}
+            </span>
           </div>
         </div>
         <div className={styles.wrapper_price}>
-          {card.previousPrice !== card.currentPrice && (
+          {card.product.previousPrice !== card.product.currentPrice && (
             <span className={cN(styles.price, styles.priceOld)}>
-              ${card.previousPrice.toFixed(2)}
+              ${card.product.previousPrice.toFixed(2)}
             </span>
           )}
-          <span className={styles.price}>${card.currentPrice.toFixed(2)}</span>
+          <span className={styles.price}>
+            ${card.product.currentPrice.toFixed(2)}
+          </span>
         </div>
         <div className={cN(styles.wrapper_price, styles.wrapper_priceSubtotal)}>
           <h5 className={styles.priceSubtotal_title}>Subtotal</h5>
-          <span className={styles.priceSubtotal_total}>$??.??</span>
+          <span className={styles.priceSubtotal_total}>
+            ${(card.product.currentPrice * card.cartQuantity).toFixed(2)}
+          </span>
         </div>
-        <div className={styles.quantity}>- 1 +</div>
+        <div className={styles.quantity}>- {card.cartQuantity} +</div>
         <button type="button" className={styles.btnClose} onClick={() => {}}>
           <CloseIcon className={styles.iconClose} />
         </button>
