c4f7ede2fe1ffc80531f604b077e87f4351a8c94
Vitaliy Snezhkin <vpsnejok@gmail.com>
2023-11-09 15:42:33 +0200

    fixed Header styles

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -10,13 +10,11 @@ import LoginForm from '../LoginForm/LoginForm';
 import Nav from '../Nav/Nav';
 import Search from '../Search/Search';
 
-
 // ICONS IMPORT:
 import LogoIcon from './icons/LogoIcon';
 import WishlistIcon from './icons/WishlistIcon';
 import CartIcon from './icons/CartIcon';
 import LoginIcon from './icons/LoginIcon';
-
 
 // USER IMPORTS:
 import {
@@ -27,7 +25,6 @@ import { useLoginUserMutation } from '../../store/serverResponse/danitApi.auth';
 
 import styles from './Header.module.scss';
 
-
 function Header(props) {
   const {
     actions: {
@@ -36,8 +33,6 @@ function Header(props) {
       getWishlist,
     },
   } = props;
-
-
 
   /* --------------------------- INIT HOOKS: --------------------------- */
 
@@ -120,7 +115,7 @@ function Header(props) {
             <span className={cn(styles.line, styles.shortLine)} />
             <span className={styles.line} />
           </span>
-          <Link to="/">
+          <Link to="/" className={styles.logoLink}>
             <LogoIcon />
             <span className={styles.header_logoTitle}>Bud</span>
             <span className={cn(styles.header_logoTitle, styles.accentColor)}>Wellness</span>
@@ -147,12 +142,12 @@ function Header(props) {
         </div>
         {showModal && (
           isUserLogin
-            ?
-            <button className={styles.header_userMenu} onClick={logoutHandler}>Logout</button>
-            :
-            <Modal handleModal={handleModal}>
-              <LoginForm actions={{ setShowModal, getCart, getWishlist }} />
-            </Modal>
+            ? <button className={styles.header_userMenu} onClick={logoutHandler}>Logout</button>
+            : (
+              <Modal handleModal={handleModal}>
+                <LoginForm actions={{ setShowModal, getCart, getWishlist }} />
+              </Modal>
+            )
         )}
       </Container>
     </header>
