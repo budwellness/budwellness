6737de27ebe5da4376f0c0e65ae4ec98e875920b
Vitaliy Snezhkin <vpsnejok@gmail.com>
2023-11-11 03:12:16 +0200

    fixed app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,8 @@
 /* eslint-disable */
-import React, {useState, useEffect} from 'react';
-import {Routes, Route} from 'react-router-dom';
-import {useDispatch, useSelector} from 'react-redux';
-import {ToastContainer} from 'react-toastify';
+import React, { useState, useEffect } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { ToastContainer } from 'react-toastify';
 
 // COMPONENT IMPORTS:
 import Header from './components/Header/Header';
@@ -14,155 +14,149 @@ import CartPage from './pages/CartPage/CartPage';
 import NotFoundPage from './pages/NotFoundPage/NotFoundPage';
 import TestForBackPage from './pages/TestForBackPage/TestForBackPage';
 import CartModal from './components/CartModal/CartModal';
-import Footer from './components/Footer/Footer'
-import Modal from "./components/Modal/Modal";
-import LoginForm from "./components/LoginForm/LoginForm";
+import Footer from './components/Footer/Footer';
 
 // USER IMPORTS:
 import {
-    useLoginUserMutation
-} from './store/serverResponse/danitApi.auth'
+  useLoginUserMutation,
+} from './store/serverResponse/danitApi.auth';
 
 import {
-    userLoginUserAction, userLogutUserAction,
-} from './store/user/user.slice'
+  userLoginUserAction, userLogutUserAction,
+} from './store/user/user.slice';
 
 // WISHLIST IMPORTS:
-import {setWishlistAction} from './store/wishlist/wishList.slice';
+import { setWishlistAction } from './store/wishlist/wishList.slice';
 
 import {
-    useLazyGetWishlistQuery,
-} from './store/serverResponse/danitApi.wishlist'
+  useLazyGetWishlistQuery,
+} from './store/serverResponse/danitApi.wishlist';
 
 // CART IMPORTS:
-import {setCartAction} from './store/cart/cart.slice';
+import { setCartAction } from './store/cart/cart.slice';
 import {
-    useLazyGetCartQuery,
-} from './store/serverResponse/danitApi.cart'
-
+  useLazyGetCartQuery,
+} from './store/serverResponse/danitApi.cart';
 
 // import { useGetAllProductsQuery } from './store/serverResponse/fetchLocalJson';
 
 import './App.scss';
 import 'react-toastify/dist/ReactToastify.css';
 
-import {useGetAllProductsQuery} from './store/serverResponse/danitApi.products';
-
-
-const log = console.log
+import { useGetAllProductsQuery } from './store/serverResponse/danitApi.products';
 import ContactPage from './pages/ContactPage/ContactPage';
-import styles from "./components/Header/Header.module.scss";
+import styles from './components/Header/Header.module.scss';
 
+const { log } = console;
 
 function App() {
+  const { data: products, error } = useGetAllProductsQuery();
+  const [showModal, setShowModal] = useState(false);
+  const [showCartModal, setShowCartModal] = useState(false);
 
-    const {data: products, error} = useGetAllProductsQuery();
-    const [showModal, setShowModal] = useState(false);
-    const [showCartModal, setShowCartModal] = useState(false);
+  /* --------------------------- REDUX STATE: --------------------------- */
+  const { isUserLogin } = useSelector(
+    (state) => state.user,
+  );
 
+  /* --------------------------- INIT HOOKS: --------------------------- */
 
-    /* --------------------------- REDUX STATE: --------------------------- */
-    const {isUserLogin} = useSelector(
-        (state) => state.user
-    );
+  const dispatch = useDispatch();
 
-    /* --------------------------- INIT HOOKS: --------------------------- */
+  /* --------------------------- RTK QUERY CUSTOM HOOKS: --------------------------- */
 
-    const dispatch = useDispatch();
+  // WISHLIST API:
+  const [
+    getWishlist,
+    { data: userWishListData, isSuccess: isSuccessUserWishlistData },
+  ] = useLazyGetWishlistQuery();
 
-    /* --------------------------- RTK QUERY CUSTOM HOOKS: --------------------------- */
+  const [
+    getCart,
+    {
+      data: userCartData,
+      isSuccess: isSuccessUserCartData,
+    },
+  ] = useLazyGetCartQuery();
 
+  /* --------------------------- COMPONENT LOGIC: --------------------------- */
 
-    // WISHLIST API:
-    const [
-        getWishlist,
-        {data: userWishListData, isSuccess: isSuccessUserWishlistData},
-    ] = useLazyGetWishlistQuery();
-
-    const [
-        getCart,
-        {
-            data: userCartData,
-            isSuccess: isSuccessUserCartData,
-        }
-    ] = useLazyGetCartQuery();
-
-    /* --------------------------- COMPONENT LOGIC: --------------------------- */
-
-    const initUserOnLoad = () => {
-        const localStorageToken = localStorage.getItem('token');
-        if (!localStorageToken) {
-            log('User not logged in');
-        } else {
-            log('token is present, put it into store...');
-            dispatch(userLoginUserAction(localStorageToken));
-            getWishlist(localStorageToken);
-            getCart(localStorageToken)
-        }
-    };
-
-    useEffect(() => initUserOnLoad(), []);
-
-    /* ------------------------------------------------ */
-
-    const initUserWishlistOnLoad = () => {
-        if (isUserLogin && userWishListData) {
-            dispatch(setWishlistAction(userWishListData.products));
-        }
-    };
-
-    /* ------------------------------------------------ */
-
-    /* --------------------------- COMPONENT HELPER HANDLERS: --------------------------- */
-
-    const handleModal = () => {
-        setShowModal(!showModal);
-    };
-
-    const logoutHandler = () => {
-        localStorage.removeItem('token');
-        dispatch(userLogutUserAction());
-    };
-
-    /* ------------------------------------------------ */
-
-
-    const initUserCardOnLoad = () => {
-        if (isUserLogin && userCartData) {
-            dispatch(setCartAction(userCartData.products))
-        }
+  const initUserOnLoad = () => {
+    const localStorageToken = localStorage.getItem('token');
+    if (!localStorageToken) {
+      log('User not logged in');
+    } else {
+      log('token is present, put it into store...');
+      dispatch(userLoginUserAction(localStorageToken));
+      getWishlist(localStorageToken);
+      getCart(localStorageToken);
     }
+  };
 
-    useEffect(() => initUserCardOnLoad(), [isSuccessUserCartData])
+  useEffect(() => initUserOnLoad(), []);
 
-    useEffect(() => initUserWishlistOnLoad(), [isSuccessUserWishlistData]);
+  /* ------------------------------------------------ */
 
-    return (
-        <>
-            <ToastContainer
-                position="bottom-right"
-                autoClose={3000}
-                theme="colored"
-            />
-            <Header actions={{handleModal, showModal, setShowModal, setShowCartModal, getCart, getWishlist}}/>
-            <Routes>
-                <Route path="/" element={<HomePage/>}/>
-                <Route path="/shop" element={<ProductsPage/>}/>
-                <Route path="/product/:productID" element={<SingleProductPage/>}/>
-                <Route path="/wishlist" element={<WishlistPage/>}/>
-                <Route path="/cart" element={<CartPage/>}/>
-                <Route path="/contact" element={<ContactPage/>}/>
-                <Route path="/test" element={<TestForBackPage/>}/>
-                <Route path="*" element={<NotFoundPage/>}/>
-            </Routes>
-            <Footer/>
-            <CartModal
-                showCartModal={showCartModal}
-                setShowCartModal={setShowCartModal}
-            />
-            {showCartModal && <div className="overLayCartModal" onClick={() => setShowCartModal(false)}/>}
-        </>
-    );
+  const initUserWishlistOnLoad = () => {
+    if (isUserLogin && userWishListData) {
+      dispatch(setWishlistAction(userWishListData.products));
+    }
+  };
+
+  /* ------------------------------------------------ */
+
+  /* --------------------------- COMPONENT HELPER HANDLERS: --------------------------- */
+
+  const handleModal = () => {
+    setShowModal(!showModal);
+  };
+
+  const logoutHandler = () => {
+    localStorage.removeItem('token');
+    dispatch(userLogutUserAction());
+  };
+
+  /* ------------------------------------------------ */
+
+  const initUserCardOnLoad = () => {
+    if (isUserLogin && userCartData) {
+      dispatch(setCartAction(userCartData.products));
+    }
+  };
+
+  useEffect(() => initUserCardOnLoad(), [isSuccessUserCartData]);
+
+  useEffect(() => initUserWishlistOnLoad(), [isSuccessUserWishlistData]);
+
+  return (
+    <>
+      <ToastContainer
+        position="bottom-right"
+        autoClose={3000}
+        theme="colored"
+      />
+      <Header actions={{
+        handleModal, showModal, setShowModal, setShowCartModal, getCart, getWishlist,
+      }}
+      />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/shop" element={<ProductsPage />} />
+        <Route path="/product/:productID" element={<SingleProductPage />} />
+        <Route path="/wishlist" element={<WishlistPage />} />
+        <Route path="/cart" element={<CartPage />} />
+        <Route path="/contact" element={<ContactPage />} />
+        <Route path="/test" element={<TestForBackPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+      <Footer />
+      <CartModal
+        showCartModal={showCartModal}
+        setShowCartModal={setShowCartModal}
+      />
+      {showCartModal && <div className="overLayCartModal" onClick={() => setShowCartModal(false)} />}
+    </>
+  );
 }
 
 export default App;
