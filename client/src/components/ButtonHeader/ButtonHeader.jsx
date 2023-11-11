352906e488360972123709e42dddf691b8bf50ec
Vitaliy Snezhkin <vpsnejok@gmail.com>
2023-11-11 02:50:06 +0200

    create component ButtonHeader

--- client/src/components/ButtonHeader/ButtonHeader.jsx
+++ client/src/components/ButtonHeader/ButtonHeader.jsx
@@ -1,36 +1,45 @@
-import cn from "classnames";
-import PropTypes from "prop-types";
+import React from 'react';
+import cn from 'classnames';
+import PropTypes from 'prop-types';
 
-import styles from "./ButtonHeader.module.scss"
+import styles from './ButtonHeader.module.scss';
 
-const Button = ({className, type, onClick, children, href, ...restProps}) => {
+function Button({
+  className, type, onClick, children, href, ...restProps
+}) {
+  const Component = href ? 'a' : 'button';
 
-    let Component = href ? "a" : "button";
-
-    return (
-        <Component
-            className={cn(styles.btn, className)}
-            {...restProps}
-            href={href}
-            type={href ? undefined : type}
-            onClick={onClick}>
-            {children}
-        </Component>
-    )
+  return (
+    <Component
+      className={cn(styles.btn, className)}
+            // eslint-disable-next-line react/jsx-props-no-spreading
+      {...restProps}
+      href={href}
+      type={href ? undefined : type}
+      onClick={onClick}
+    >
+      {children}
+    </Component>
+  );
 }
 
-export default Button;
-
 Button.defaultProps = {
-    onClick: () => {
-    },
-    type: "button",
+  onClick: () => {
+  },
+  type: 'button',
+  href: '',
+  to: '',
+  className: '',
+  children: null,
 };
 
 Button.propTypes = {
-    onClick: PropTypes.func,
-    href: PropTypes.string,
-    type: PropTypes.string,
-    to: PropTypes.string,
-    className: PropTypes.string,
+  onClick: PropTypes.func,
+  href: PropTypes.string,
+  type: PropTypes.string,
+  to: PropTypes.string,
+  className: PropTypes.string,
+  children: PropTypes.node,
 };
+
+export default Button;
