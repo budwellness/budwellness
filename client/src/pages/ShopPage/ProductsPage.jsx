065bb72b58c93e5808c23db30b5a8fa8c542bf5c
Diana Liashenko <leonidovna8@gmail.com>
2023-11-11 15:01:49 +0200

    update shoppage to actual dev

--- client/src/pages/ShopPage/ProductsPage.jsx
+++ client/src/pages/ShopPage/ProductsPage.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import Container from '../../components/Container/Container';
 import PopularProducts from '../../components/PopularProducts/PopularProducts';
-
 import PagePreviewHeader from '../../components/PagePreviewHeader/PagePreviewHeader';
 import Filter from '../../components/Filter/Filter';
+import styles from './ProductsPage.module.scss';
+import ProductCard from '../../components/ProductCard/ProductCard';
 
 function ProductsPage() {
   return (
@@ -13,7 +14,10 @@ function ProductsPage() {
         text="Immerse yourself in a unique shopping experience, where your choice is our priority."
       />
       <Container>
-        <Filter />
+        <div className={styles.wrapperContainer}>
+          <Filter />
+          <ProductCard />
+        </div>
         <PopularProducts />
       </Container>
     </>
