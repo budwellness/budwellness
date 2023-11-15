9179743de199061cf1999231f13f9b97f8478269
Diana Liashenko <leonidovna8@gmail.com>
2023-11-16 00:07:54 +0200

    updated styles Categories

--- client/src/components/Categories/Categories.jsx
+++ client/src/components/Categories/Categories.jsx
@@ -6,38 +6,32 @@ const categoryData = [
   {
     id: 0,
     name: 'Pre-Rolls',
-    image:
-      'https://smokecones.com/cdn/shop/files/PreRoll_MegaMenu_560x.png?v=1650498552',
+    image: '/images/pre-rolls.png',
   },
   {
     id: 1,
     name: 'Flowers',
-    image:
-      'https://st4.depositphotos.com/27756932/40843/i/600/depositphotos_408434826-stock-photo-cannabis-leaf-pattern-green-hemp.jpg',
+    image: '/images/flowers.jpeg',
   },
   {
     id: 2,
     name: 'Edible',
-    image:
-      'https://plantsbeforepills.com/wp-content/uploads/2021/03/3-CBD-Baked-Treats-for-Easter.jpg',
+    image: '/images/edible.jpeg',
   },
   {
     id: 3,
     name: 'Oils',
-    image:
-      'https://st4.depositphotos.com/27756932/40966/i/600/depositphotos_409664420-stock-photo-hemp-oil-serum-glass-dropper.jpg',
+    image: '/images/oils.jpeg',
   },
   {
     id: 4,
     name: 'Beverage',
-    image:
-      'https://img.freepik.com/premium-photo/sbd-hemp-drink-different-glasses-creative-green-hard-shadow-background-relaxing-cannabis-cocktail_721474-1010.jpg?w=826',
+    image: '/images/beverage.jpg',
   },
   {
     id: 5,
     name: 'Seeds',
-    image:
-      'https://www.mcclatchy-partners.com/paradise-media/health-wellness/wcutkc/picture274866971/alternates/LANDSCAPE_1140/Copy%20of%20seed-banks-4.png',
+    image: '/images/seeds.png',
   },
 ];
 
