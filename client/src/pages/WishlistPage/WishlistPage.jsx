8e8ff9ed3c3d0644fbc331566482ba6213574f27
Diana Liashenko <leonidovna8@gmail.com>
2023-11-20 20:15:36 +0200

    wishlist

--- client/src/pages/WishlistPage/WishlistPage.jsx
+++ client/src/pages/WishlistPage/WishlistPage.jsx
@@ -1,8 +1,17 @@
-import React from 'react';
+import React, { useCallback } from 'react';
+import { useSelector } from 'react-redux';
 import PagePreviewHeader from '../../components/PagePreviewHeader/PagePreviewHeader';
 import Container from '../../components/Container/Container';
+import ProductCard from '../../components/ProductCard/ProductCard';
+import styles from './WishlistPage.module.scss';
 
 function WishlistPage() {
+  const { wishList: wishlistStoreData } = useSelector(
+    (state) => state.wishlist,
+  );
+
+  const toggleWishlistHandler = useCallback(() => {}, []);
+
   return (
     <>
       <PagePreviewHeader
@@ -10,7 +19,17 @@ function WishlistPage() {
         text="Curate your Wishlist with the finest selection of products waiting to become your favorites!"
       />
       <Container>
-        <div>wishList</div>
+        <main className={styles.container}>
+          {wishlistStoreData.map((product) => (
+            <ProductCard
+              key={product._id}
+              product={product}
+              actions={{
+                toggleWishlistHandler,
+              }}
+            />
+          ))}
+        </main>
       </Container>
     </>
   );
