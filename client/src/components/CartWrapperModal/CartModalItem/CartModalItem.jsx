1db9d46c2ab991981d455e0122f6b63973b34361
Vitaliy Snezhkin <vpsnejok@gmail.com>
2023-11-20 22:18:47 +0200

    fixed CartModalItem

--- client/src/components/CartWrapperModal/CartModalItem/CartModalItem.jsx
+++ client/src/components/CartWrapperModal/CartModalItem/CartModalItem.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import {useDispatch, useSelector} from 'react-redux';
 import PropTypes from 'prop-types';
 
-import { toast } from 'react-toastify';
+import {toast} from 'react-toastify';
 
 // COMPONENT IMPORTS:
 import ButtonCount from '../../ButtonCount/ButtonCount';
@@ -10,126 +10,143 @@ import LinkUnderline from '../../LinkUnderline/LinkUnderline';
 
 // CART IMPORTS:
 import {
-  decreaseCartItemQuantityAction,
-  increaseCartItemQuantityAction,
+    decreaseCartItemQuantityAction,
+    increaseCartItemQuantityAction,
 } from '../../../store/cart/cart.slice';
 import {
-  useAddToCartMutation,
-  useDecreaseCartQuantityMutation,
+    useAddToCartMutation,
+    useDecreaseCartQuantityMutation,
 } from '../../../store/serverResponse/danitApi.cart';
 
 import styles from './CartModalItem.module.scss';
+import {Link} from "react-router-dom";
+import {setCartModal} from "../../../store/cartModal/cartModal.slice.js";
 
-const { log } = console;
+const {log} = console;
 
 function CartModalItem(props) {
-  /* --------------------------- INIT PROPS: --------------------------- */
-  const {
-    products: {
-      cartQuantity,
-      product,
-    },
-  } = props;
+    /* --------------------------- INIT PROPS: --------------------------- */
+    const {
+        products: {
+            cartQuantity,
+            product,
+        },
+    } = props;
 
-  /* --------------------------- INIT HOOKS: --------------------------- */
+    /* --------------------------- INIT HOOKS: --------------------------- */
 
-  const dispatch = useDispatch();
+    const dispatch = useDispatch();
 
-  /* --------------------------- RTK QUERY CUSTOM HOOKS: --------------------------- */
+    /* --------------------------- RTK QUERY CUSTOM HOOKS: --------------------------- */
 
-  const [addProductToCart] = useAddToCartMutation();
-  const [decreaseCartQuantity] = useDecreaseCartQuantityMutation();
+    const [addProductToCart] = useAddToCartMutation();
+    const [decreaseCartQuantity] = useDecreaseCartQuantityMutation();
 
-  /* --------------------------- REDUX STATE: --------------------------- */
+    /* --------------------------- REDUX STATE: --------------------------- */
 
-  const { token: tokenReduxStore } = useSelector((state) => state.user);
+    const {token: tokenReduxStore} = useSelector((state) => state.user);
 
-  /* --------------------------- COMPONENT HANDLERS: --------------------------- */
+    /* --------------------------- COMPONENT HANDLERS: --------------------------- */
 
-  const increaseCartQuantityHandler = () => {
-    const requestData = {
-      productId: product._id,
-      token: tokenReduxStore,
+    const increaseCartQuantityHandler = () => {
+        const requestData = {
+            productId: product._id,
+            token: tokenReduxStore,
+        };
+        try {
+            addProductToCart(requestData);
+            dispatch(increaseCartItemQuantityAction(product._id));
+        } catch (error) {
+            log(error);
+            toast.error('Something went wrong...');
+        }
     };
-    try {
-      addProductToCart(requestData);
-      dispatch(increaseCartItemQuantityAction(product._id));
-    } catch (error) {
-      log(error);
-      toast.error('Something went wrong...');
+
+    const handleCloseCart = () => {
+        dispatch(setCartModal(false))
     }
-  };
 
-  /* ------------------------------------------------ */
+    /* ------------------------------------------------ */
 
-  const decreaseCartQuantityHandler = () => {
-    const requestData = {
-      productId: product._id,
-      token: tokenReduxStore,
+    const decreaseCartQuantityHandler = () => {
+        const requestData = {
+            productId: product._id,
+            token: tokenReduxStore,
+        };
+        try {
+            decreaseCartQuantity(requestData);
+            dispatch(decreaseCartItemQuantityAction(product._id));
+        } catch (error) {
+            log(error);
+            toast.error('Something went wrong...');
+        }
     };
-    try {
-      decreaseCartQuantity(requestData);
-      dispatch(decreaseCartItemQuantityAction(product._id));
-    } catch (error) {
-      log(error);
-      toast.error('Something went wrong...');
-    }
-  };
 
-  return (
-    <li className={styles.cartItem}>
-      <div className={styles.wrapperImg}>
-        {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-        <a href="#">
-          <img src={product.imageUrls[0]} alt={product.name} />
-        </a>
-      </div>
-      <div className={styles.main}>
-        {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-        <a href="#" className={styles.mainTitleLink}>{product.name}</a>
-        <span className={styles.mainPrice}>
-          $
-          {product.currentPrice}
-        </span>
-        <div className={styles.count}>
-          <ButtonCount
-            productQuantity={product.quantity}
-            actions={{ increaseCartQuantityHandler, decreaseCartQuantityHandler }}
-            cartQuantity={cartQuantity}
-          />
-        </div>
-        { }
-        {/* <a className={styles.removeLink} href="#">remove</a> */}
-        <LinkUnderline type="button" productId={product._id} style={{ order: 4, marginTop: '10px' }}>Remove</LinkUnderline>
-      </div>
-    </li>
-  );
+    return (
+        <li className={styles.cartItem}>
+            <div className={styles.wrapperImg}>
+                <Link
+                    to={`/product/${product.itemNo}`}
+                    onClick={handleCloseCart}
+                >
+                    <img src={product.imageUrls[0]} alt={product.name}/>
+                </Link>
+            </div>
+            <div className={styles.main}>
+                <Link
+                    to={`/product/${product.itemNo}`}
+                    className={styles.mainTitleLink}
+                    onClick={handleCloseCart}
+                >
+                    {product.name}
+                </Link>
+                <span className={styles.mainPrice}>${product.currentPrice}</span>
+                <div className={styles.count}>
+                    <ButtonCount
+                        productQuantity={product.quantity}
+                        actions={{
+                            increaseCartQuantityHandler,
+                            decreaseCartQuantityHandler,
+                        }}
+                        cartQuantity={cartQuantity}
+                    />
+                </div>
+                <LinkUnderline
+                    type="button"
+                    productId={product._id}
+                    style={{order: 4, marginTop: '10px'}}
+                >
+                    Remove
+                </LinkUnderline>
+            </div>
+        </li>
+    );
 }
 
 CartModalItem.propTypes = {
-  products: PropTypes.shape({
-    cartQuantity: PropTypes.number,
-    product: PropTypes.shape({
-      _id: PropTypes.string,
-      name: PropTypes.string,
-      imageUrls: PropTypes.arrayOf(PropTypes.string),
-      currentPrice: PropTypes.number,
-      quantity: PropTypes.number,
+    products: PropTypes.shape({
+        cartQuantity: PropTypes.number,
+        product: PropTypes.shape({
+            _id: PropTypes.string,
+            name: PropTypes.string,
+            imageUrls: PropTypes.arrayOf(PropTypes.string),
+            currentPrice: PropTypes.number,
+            quantity: PropTypes.number,
+        }),
     }),
-  }),
 };
 
 CartModalItem.defaultProps = {
-  products: {
-    cartQuantity: 0,
-    product: {
-      _id: '',
-      name: '',
-      imageUrls: [],
-      currentPrice: 0,
-      quantity: 0,
+    products: {
+        cartQuantity: 0,
+        product: {
+            _id: '',
+            name: '',
+            imageUrls: [],
+            currentPrice: 0,
+            quantity: 0,
+        },
     },
-  },
 };
 
 export default CartModalItem;
