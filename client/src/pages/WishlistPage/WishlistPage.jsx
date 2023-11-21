6169e6c4db77ee4a619c9d538418cfe7077d5517
Diana Liashenko <leonidovna8@gmail.com>
2023-11-21 23:09:46 +0200

    render products in wishlist

--- client/src/pages/WishlistPage/WishlistPage.jsx
+++ client/src/pages/WishlistPage/WishlistPage.jsx
@@ -1,16 +1,25 @@
-import React, { useCallback } from 'react';
-import { useSelector } from 'react-redux';
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import PagePreviewHeader from '../../components/PagePreviewHeader/PagePreviewHeader';
 import Container from '../../components/Container/Container';
 import ProductCard from '../../components/ProductCard/ProductCard';
 import styles from './WishlistPage.module.scss';
+import { toggleWishlistAction } from '../../store/wishlist/wishList.slice';
+import { addItemToCartAction } from '../../store/cart/cart.slice';
 
 function WishlistPage() {
+  const dispatch = useDispatch();
   const { wishList: wishlistStoreData } = useSelector(
     (state) => state.wishlist,
   );
 
-  const toggleWishlistHandler = useCallback(() => {}, []);
+  const toggleWishlistHandler = (productId) => {
+    dispatch(toggleWishlistAction(productId));
+  };
+
+  const toggleCartHandler = (productId) => {
+    dispatch(addItemToCartAction(productId));
+  };
 
   return (
     <>
@@ -20,15 +29,17 @@ function WishlistPage() {
       />
       <Container>
         <main className={styles.container}>
-          {wishlistStoreData.map((product) => (
-            <ProductCard
-              key={product._id}
-              product={product}
-              actions={{
-                toggleWishlistHandler,
-              }}
-            />
-          ))}
+          {Array.isArray(wishlistStoreData)
+            && wishlistStoreData.map((product) => (
+              <ProductCard
+                key={product._id}
+                product={product}
+                actions={{
+                  toggleWishlistHandler: () => toggleWishlistHandler(product._id),
+                  toggleCartHandler: () => toggleCartHandler(product._id),
+                }}
+              />
+            ))}
         </main>
       </Container>
     </>
