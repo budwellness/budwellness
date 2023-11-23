f4b1d79509c2ec3c5f82c7acb0fdccdf71ce8014
User <tatiana.naumchuk123456789@gmail.com>
2023-11-23 03:52:06 +0200

    Our Team component done

--- client/src/pages/OurTeam/OurTeam.jsx
+++ client/src/pages/OurTeam/OurTeam.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable */
 
 import React from 'react';
+import cN from 'classnames';
 // import { Link } from 'react-router-dom';
 import PagePreviewHeader from '../../components/PagePreviewHeader/PagePreviewHeader';
 import Container from '../../components/Container/Container';
@@ -11,6 +12,7 @@ import SocialNetworks from './SocialNetworks/SocialNetworks';
 import styles from './OurTeam.module.scss';
 
 function OurTeam() {
+  // const hoverClass = 'hover';
   return (
     <>
       <PagePreviewHeader
@@ -20,91 +22,106 @@ function OurTeam() {
       <Container>
         <div className={styles.wrapper}>
           <div className={styles.wrapper__person}>
-            <SocialNetworks />
+            <div className={styles.personHover}>
+              <SocialNetworks hover={styles.hoverClass} />
+            </div>
             <img
-              src="/images/Ihor.jpg"
+              src="/images/Stanislav.png"
               alt="photo"
               className={styles.photoItem}
             />
-            <div className={styles.wpapperLink}>{/* <SocialNetworks /> */}</div>
+            <h3 className={styles.name}>Stanislav Ostapushchenko</h3>
+          </div>
+          <div className={styles.wrapper__person}>
+            {/* <div className={styles.wpapper__photo}> */}
+            <div className={styles.personHover}>
+              <SocialNetworks hover={styles.hoverClass} />
+            </div>
+            <img
+              src="/images/Diana.png"
+              alt="photo"
+              className={styles.photoItem}
+            />
+            {/* </div> */}
+            <h3 className={styles.name}>Diana Liashenko</h3>
+          </div>
+          <div className={styles.wrapper__person}>
+            {/* <div className={styles.wpapper__photo}> */}
+            <div className={styles.personHover}>
+              <SocialNetworks hover={styles.hoverClass} />
+            </div>
+            <img
+              src="/images/Serhii.png"
+              alt="photo"
+              className={styles.photoItem}
+            />
+            {/* </div> */}
+            <h3 className={styles.name}>Serhii Sokol</h3>
+          </div>
+          <div className={styles.wrapper__person}>
+            {/* <div className={styles.wpapper__photo}> */}
+            <div className={styles.personHover}>
+              <SocialNetworks hover={styles.hoverClass} />
+            </div>
+            <img
+              src="/images/Vitaliy.png"
+              alt="photo"
+              className={styles.photoItem}
+            />
+            {/* </div> */}
+            <h3 className={styles.name}>Vitaliy Snezhkin</h3>
+          </div>
+          <div className={styles.wrapper__person}>
+            {/* <div className={styles.wpapper__photo}> */}
+            <div className={styles.personHover}>
+              <SocialNetworks hover={styles.hoverClass} />
+            </div>
+            <img
+              src="/images/Ihor.png"
+              alt="photo"
+              className={styles.photoItem}
+            />
+            {/* </div> */}
             <h3 className={styles.name}>Ihor Toroshanko</h3>
           </div>
           <div className={styles.wrapper__person}>
-            <div className={styles.wpapper__photo}>
-              <img
-                src="/images/Ruslan.jpg"
-                alt="photo"
-                className={styles.photoItem}
-              />
-              <SocialNetworks />
+            {/* <div className={styles.wpapper__photo}> */}
+            <div className={styles.personHover}>
+              <SocialNetworks hover={styles.hoverClass} />
             </div>
+            <img
+              src="/images/Ruslan.png"
+              alt="photo"
+              className={styles.photoItem}
+            />
+            {/* </div> */}
             <h3 className={styles.name}>Ruslan Kulahin</h3>
           </div>
           <div className={styles.wrapper__person}>
-            <div className={styles.wpapper__photo}>
-              <img
-                src="/images/Tanya.jpg"
-                alt="photo"
-                className={styles.photoItem}
-              />
-              <SocialNetworks />
+            {/* <div className={styles.wpapper__photo}> */}
+            <div className={styles.personHover}>
+              <SocialNetworks hover={styles.hoverClass} />
             </div>
+            <img
+              src="/images/Tanya.png"
+              alt="photo"
+              className={styles.photoItem}
+            />
+            {/* </div> */}
             <h3 className={styles.name}>Tetiana Naumchuk</h3>
           </div>
           <div className={styles.wrapper__person}>
-            <div className={styles.wpapper__photo}>
-              <img
-                src="/images/Tanya_2.jpg"
-                alt="photo"
-                className={styles.photoItem}
-              />
-              <SocialNetworks />
+            {/* <div className={styles.wpapper__photo}> */}
+            <div className={styles.personHover}>
+              <SocialNetworks hover={styles.hoverClass} />
             </div>
-            <h3 className={styles.name}>Tetiana Naumchuk</h3>
-          </div>
-          <div className={styles.wrapper__person}>
-            <div className={styles.wpapper__photo}>
-              <img
-                src="/images/Tanya.jpg"
-                alt="photo"
-                className={styles.photoItem}
-              />
-              <SocialNetworks />
-            </div>
-            <h3 className={styles.name}>Tetiana Naumchuk</h3>
-          </div>
-          <div className={styles.wrapper__person}>
-            <div className={styles.wpapper__photo}>
-              <img
-                src="/images/Ruslan.jpg"
-                alt="photo"
-                className={styles.photoItem}
-              />
-              <SocialNetworks />
-            </div>
-            <h3 className={styles.name}>Ruslan Kulahin</h3>
-          </div>
-          <div className={styles.wrapper__person}>
-            <div className={styles.wpapper__photo}>
-              <img
-                src="/images/Ihor.jpg"
-                alt="photo"
-                className={styles.photoItem}
-              />
-              <SocialNetworks />
-            </div>
-            <h3 className={styles.name}>Ihor Toroshanko</h3>
-          </div>
-          <div className={styles.wrapper__person}>
-            <div className={styles.wpapper__photo}>
-              <img
-                src="/images/Tanya_2.jpg"
-                alt="photo"
-                className={styles.photoItem}
-              />
-              <SocialNetworks />
-            </div>
-            <h3 className={styles.name}>Tetiana Naumchuk</h3>
+            <img
+              src="/images/Denis.png"
+              alt="photo"
+              className={styles.photoItem}
+            />
+            {/* </div> */}
+            <h3 className={styles.name}>Denis Gavrilenko</h3>
           </div>
         </div>
       </Container>
