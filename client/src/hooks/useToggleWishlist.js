4f0cbad98cc818eb098341a70b7ab94a13ffdd23
Stanislav <stanjke@gmail.com>
2023-11-24 02:19:21 +0200

    testing extra logic to add wishlist unauth

--- client/src/hooks/useToggleWishlist.js
+++ client/src/hooks/useToggleWishlist.js
@@ -17,30 +17,39 @@ const useToggleWishlist = () => {
   const [addProductToWishlist] = useAddProductToWishlistMutation();
   const toggleWishlist = async (product, token, wishlistStoreData) => {
     const isExist = wishlistStoreData.some((p) => p._id === product._id);
-    if (isExist) {
-      try {
-        await removeProductFromWishlist({ productId: product._id, token })
-          .unwrap()
-          .then(() => {
-            dispatch(removeItemFromWishListAction(product));
-            toast.warn('Product was removed from wishlist!');
-          });
-      } catch (error) {
-        log(error);
-        toast.error('Something went wrong...');
+    if (token) {
+      if (isExist) {
+        try {
+          await removeProductFromWishlist({ productId: product._id, token })
+            .unwrap()
+            .then(() => {
+              dispatch(removeItemFromWishListAction(product));
+              toast.warn('Product was removed from wishlist!');
+            });
+        } catch (error) {
+          log(error);
+          toast.error('Something went wrong...');
+        }
+      } else {
+        try {
+          await addProductToWishlist({ productId: product._id, token })
+            .unwrap()
+            .then(() => {
+              dispatch(addItemToWishListAction(product));
+              toast.success('Product was added to wishlist!');
+            });
+        } catch (error) {
+          log(error);
+          toast.error('Something went wrong...');
+        }
       }
     } else {
-      try {
-        await addProductToWishlist({ productId: product._id, token })
-          .unwrap()
-          .then(() => {
-            dispatch(addItemToWishListAction(product));
-            toast.success('Product was added to wishlist!');
-          });
-      } catch (error) {
-        log(error);
-        toast.error('Something went wrong...');
+      if (isExist) {
+        dispatch(removeItemFromWishListAction(product));
+      } else {
+        dispatch(addItemToWishListAction(product));
       }
+      log('я работаю даже без токена');
     }
   };
 
