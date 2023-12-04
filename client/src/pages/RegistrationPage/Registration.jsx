6ef0469e9f12c579cf26926b55c4171c68615ca0
Vitaliy Snezhkin <vpsnejok@gmail.com>
2023-12-04 14:16:46 +0200

    update validation

--- client/src/pages/RegistrationPage/Registration.jsx
+++ client/src/pages/RegistrationPage/Registration.jsx
@@ -1,130 +1,128 @@
 import React from 'react';
-import {Form, Formik} from 'formik';
+import { Form, Formik } from 'formik';
 
-import {useNavigate} from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 import Input from './Input/Input';
 // import Textarea from './Textarea/Textarea';
 
 import styles from './Registration.module.scss';
-import {validationSchema} from "./validationRegistrationForm.js"
+import { validationSchema } from './validationRegistrationForm.js';
 
 import Container from '../../components/Container/Container';
 import ButtonHeader from '../../components/ButtonHeader/ButtonHeader';
-import {useRegistrationUserMutation} from "../../store/serverResponse/danitApi.auth.js";
-import {setModal} from "../../store/modal/modal.slice.js";
-import {useDispatch} from "react-redux";
+import { useRegistrationUserMutation } from '../../store/serverResponse/danitApi.auth.js';
+import { setModal } from '../../store/modal/modal.slice.js';
 
 function Registration() {
-const dispatch = useDispatch()
-    const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-    const [registrationUser, {data: registrationUserData, error}] = useRegistrationUserMutation();
+  const [registrationUser, { data: registrationUserData, error }] = useRegistrationUserMutation();
 
-    const initialValues = {
-        firstName: "",
-        lastName: "",
-        login: "",
-        email: "",
-        password: "",
-        telephone: "+380",
-        age: ""
-    };
+  const initialValues = {
+    firstName: '',
+    lastName: '',
+    login: '',
+    email: '',
+    password: '',
+    telephone: '+380',
+    age: '',
+  };
 
-
-    return (
-        <Container>
-            <Formik
-                initialValues={initialValues}
-                validationSchema={validationSchema}
-                onSubmit={(values) => {
-                    console.log(values);
-                    registrationUser(values);
-                    navigate("/")
-                    dispatch(setModal(true))
-                }}
-            >
-                {({}) => (
-                    <Form className={styles.wrapperForm}>
-                        <div className={styles.form}>
-                            <h3 className={styles.registration}>Registration</h3>
-                            <Input
-                                name="firstName"
-                                type="text"
-                                placeholder="Enter your name"
-                                label="Your name"
-                                className={styles.formLabel}
-                            />
-                            <Input
-                                name="lastName"
-                                type="text"
-                                placeholder="Enter your Last name"
-                                label="Your Last name"
-                                className={styles.formLabel}
-                            />
-                            <Input
-                                name="login"
-                                type="text"
-                                placeholder="Enter your Login"
-                                label="Your Login"
-                                className={styles.formLabel}
-                            />
-                            <Input
-                                name="email"
-                                type="text"
-                                placeholder="Enter your email"
-                                label="Your email"
-                                className={styles.formLabel}
-                            />
-                            <Input
-                                name="password"
-                                type="password"
-                                placeholder="Enter your password"
-                                label="Your password"
-                                className={styles.formLabel}
-                            />
-                            <Input
-                                name="repeatPassword"
-                                type="password"
-                                placeholder="Repeat password"
-                                label="Repeat your password"
-                                className={styles.formLabel}
-                            />
-                            <Input
-                                name="telephone"
-                                type="text"
-                                placeholder="Enter your phone number"
-                                label="Your phone number"
-                                className={styles.formLabel}
-                            />
-                            <Input
-                                name="age"
-                                type="text"
-                                placeholder="Enter your age"
-                                label="Your age"
-                                className={styles.formLabel}
-                            />
-                            <ButtonHeader
-                                className={styles.btnForm}
-                                type="submit"
-                                // disabled={!isValid}
-                            >
-                                Sign in
-                            </ButtonHeader>
-                            {/* <button */}
-                            {/*    type='submit' */}
-                            {/*    className={styles.btnForm} */}
-                            {/*    disabled={!isValid} */}
-                            {/* >Sign up */}
-                            {/* </button> */}
-                            {/* <button type='button' className={styles.btnForm}> */}
-                            {/*    Скасувати оформлення */}
-                            {/* </button> */}
-                        </div>
-                    </Form>
-                )}
-            </Formik>
-        </Container>
-    );
+  return (
+    <Container>
+      <Formik
+        initialValues={initialValues}
+        validationSchema={validationSchema}
+        onSubmit={(values) => {
+          console.log(values);
+          registrationUser(values);
+          navigate('/');
+          dispatch(setModal(true));
+        }}
+      >
+        {({ isValid }) => (
+          <Form className={styles.wrapperForm}>
+            <div className={styles.form}>
+              <h3 className={styles.registration}>Registration</h3>
+              <Input
+                name="firstName"
+                type="text"
+                placeholder="Enter your name"
+                label="Your name"
+                className={styles.formLabel}
+              />
+              <Input
+                name="lastName"
+                type="text"
+                placeholder="Enter your Last name"
+                label="Your Last name"
+                className={styles.formLabel}
+              />
+              <Input
+                name="login"
+                type="text"
+                placeholder="Enter your Login"
+                label="Your Login"
+                className={styles.formLabel}
+              />
+              <Input
+                name="email"
+                type="text"
+                placeholder="Enter your email"
+                label="Your email"
+                className={styles.formLabel}
+              />
+              <Input
+                name="password"
+                type="password"
+                placeholder="Enter your password"
+                label="Your password"
+                className={styles.formLabel}
+              />
+              <Input
+                name="repeatPassword"
+                type="password"
+                placeholder="Repeat password"
+                label="Repeat your password"
+                className={styles.formLabel}
+              />
+              <Input
+                name="telephone"
+                type="text"
+                placeholder="Enter your phone number"
+                label="Your phone number"
+                className={styles.formLabel}
+              />
+              <Input
+                name="age"
+                type="text"
+                placeholder="Enter your age"
+                label="Your age"
+                className={styles.formLabel}
+              />
+              <ButtonHeader
+                className={styles.btnForm}
+                type="submit"
+              >
+                Sign in
+              </ButtonHeader>
+              {/* <button */}
+              {/*    type='submit' */}
+              {/*    className={styles.btnForm} */}
+              {/*    disabled={!isValid} */}
+              {/* >Sign up */}
+              {/* </button> */}
+              {/* <button type='button' className={styles.btnForm}> */}
+              {/*    Скасувати оформлення */}
+              {/* </button> */}
+            </div>
+          </Form>
+        )}
+      </Formik>
+    </Container>
+  );
 }
 
 Registration.propTypes = {};
--- client/src/store/serverResponse/danitApi.auth.js
+++ client/src/store/serverResponse/danitApi.auth.js
@@ -1,36 +1,35 @@
-import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import * as API from '../../configs/API';
 
 export const danitApiAuth = createApi({
-    reducerPath: 'danitApiAuth',
-    baseQuery: fetchBaseQuery({
-        baseUrl: API.API_URL,
+  reducerPath: 'danitApiAuth',
+  baseQuery: fetchBaseQuery({
+    baseUrl: API.API_URL,
+  }),
+  endpoints: (build) => ({
+    loginUser: build.mutation({
+      query: ({ ...credentials }) => ({
+        url: API.API_LOGIN,
+        method: 'POST',
+        headers: {
+          'Content-type': 'application/json',
+        },
+        body: credentials,
+      }),
+      transformResponse: (response) => response.token,
     }),
-    endpoints: (build) => ({
-        loginUser: build.mutation({
-            query: ({...credentials}) => ({
-                url: API.API_LOGIN,
-                method: 'POST',
-                headers: {
-                    'Content-type': 'application/json',
-                },
-                body: credentials,
-            }),
-            transformResponse: (response) => response.token,
-        }),
-        registrationUser: build.mutation({
-            query: ({...userData}) => ({
-                url: API.API_REGISTRATION_USER,
-                method: 'POST',
-                headers: {
-                    'Content-type': 'application/json',
-                },
-                body: userData,
-            }),
+    registrationUser: build.mutation({
+      query: ({ ...userData }) => ({
+        url: API.API_REGISTRATION_USER,
+        method: 'POST',
+        headers: {
+          'Content-type': 'application/json',
+        },
+        body: userData,
+      }),
 
-        }),
     }),
+  }),
 });
 
-export const {useLoginUserMutation, useRegistrationUserMutation} = danitApiAuth;
-
+export const { useLoginUserMutation, useRegistrationUserMutation } = danitApiAuth;
