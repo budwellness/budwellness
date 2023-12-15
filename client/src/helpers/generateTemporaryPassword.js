71677f112b880c69e731d4d9e5fe572f8bc55750
Сергій Сокол <sokol.serhii@gmail.com>
2023-12-16 01:37:47 +0200

    Eslint-disable-next-line no-plusplus

--- client/src/helpers/generateTemporaryPassword.js
+++ client/src/helpers/generateTemporaryPassword.js
@@ -3,6 +3,7 @@ const generateTemporaryPassword = (length = 8) => {
     'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
   let temporaryPassword = '';
 
+  // eslint-disable-next-line no-plusplus
   for (let i = 0; i < length; i++) {
     const randomIndex = Math.floor(Math.random() * charset.length);
     temporaryPassword += charset.charAt(randomIndex);
