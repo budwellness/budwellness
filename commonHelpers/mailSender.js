f8658b778d1cc31704f793f0d1a11a6df76fadfe
Stanislav <stanjke@gmail.com>
2023-12-17 16:03:50 +0200

    change back hardcoded nodemailer credentials to dynamic

--- commonHelpers/mailSender.js
+++ commonHelpers/mailSender.js
@@ -7,30 +7,27 @@ module.exports = async (subscriberMail, letterSubject, letterHtml, res) => {
 
   //authorization for sending email
   let transporter = nodemailer.createTransport({
-    host: "smtp-mail.outlook.com", // hostname
-    secureConnection: false, // TLS requires secureConnection to be false
-    port: 587, // port for secure SMTP
-    auth: {
-      user: "budwellnessproject@outlook.com",
-      pass: "Qazwsx#123BUDWELLNESS",
-    },
-    tls: {
-      ciphers: "SSLv3",
-    },
-    // service: "outlook",
-    // service:
-    //   process.env.NODE_ENV === "production"
-    //     ? configs.production.email.mailService
-    //     : configs.development.email.mailService,
+    // host: "smtp-mail.outlook.com", // hostname
+    // secureConnection: false, // TLS requires secureConnection to be false
+    // port: 587, // port for secure SMTP
     // auth: {
-    //   user: process.env.NODE_ENV === "production" ? configs.production.email.mailUser : configs.development.email.mailUser,
-    //   pass: process.env.NODE_ENV === "production" ? configs.production.email.mailPassword : configs.development.email.mailPassword,
+    //   user: "budwellnessproject@outlook.com",
+    //   pass: "Qazwsx#123BUDWELLNESS",
     // },
+    // tls: {
+    //   ciphers: "SSLv3",
+    // },
+    // service: "outlook",
+    service: process.env.NODE_ENV === "production" ? configs.production.email.mailService : configs.development.email.mailService,
+    auth: {
+      user: process.env.NODE_ENV === "production" ? configs.production.email.mailUser : configs.development.email.mailUser,
+      pass: process.env.NODE_ENV === "production" ? configs.production.email.mailPassword : configs.development.email.mailPassword,
+    },
   });
 
   const mailOptions = {
-    // from: process.env.NODE_ENV === "production" ? configs.production.email.mailUser : configs.development.email.mailUser,
-    from: "budwellnessproject@outlook.com",
+    from: process.env.NODE_ENV === "production" ? configs.production.email.mailUser : configs.development.email.mailUser,
+    // from: "budwellnessproject@outlook.com",
     to: subscriberMail,
     subject: letterSubject,
     html: letterHtml,
