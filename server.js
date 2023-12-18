e516e6ceb50ee5d166d286ae46cb6cedba6cf75e
Stanislav <stanjke@gmail.com>
2023-12-18 18:02:48 +0200

    added backend files

--- server.js
+++ server.js
@@ -74,10 +74,10 @@ app.use("/api/partners", partners);
 // Server static assets if in production
 if (process.env.NODE_ENV === "production") {
   // Set static folder
-  app.use(express.static("client/build"));
+  app.use(express.static("client/dist"));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
   });
 }
 
